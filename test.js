07500002b8084838a6c492c21a1f0b38f35df088
imran-ali <146611498+FastWebSul@users.noreply.github.com>
2023-11-21 10:58:40 +0500

    s

---
+++ test.js
@@ -0,0 +1,106 @@
+// const accountId=  1234
+// var name= "imran"
+// let age = "34"
+// console.table([accountId, name, age])
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+// let  Score = 33
+// console.log(typeof Score)
+// let Score = "33abc"
+// console.log( typeof Score)
+// let ValueInNumber = Number(Score)
+// console.log(typeof ValueInNumber)
+// // console.log(ValueInNumber)
+
+// let Score = "imran"
+// console.log( typeof Score)
+// let ValueInNumber = Number(Score)
+// console.log(typeof ValueInNumber)
+// console.log(ValueInNumber)
+
+// let isLoggedIn = 0 
+// let BooleanIsLogedIn = Boolean(isLoggedIn)
+// console.log( BooleanIsLogedIn)
+
+//  let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+// ****************************************************
+let value = 33
+let negValue = - value
+// console.log(negValue)
+// **************************
+
+// let str1 = "imran"
+// let str2 = "ali"
+
+//  let str3 = str1+ str2
+//  console.log(str3)
+
+// console.log(+"")
+//   let gameCounter = 100
+//   ++gameCounter;
+//   console.log(gameCounter)
+//   let gameCounter = 100
+//   gameCounter++;
+//   console.log(gameCounter)
+// ******************************************
+
+
+//  console.log(2 > 1)
+//  console.log(2 < 1)
+//  console.log(null > 1)
+
+
+// data types 
+// ********************
+// primitive 
+// 7 types 
+// String
+// Number
+// Boolean
+// null
+// undefined
+// Symbol
+// BigInt
+// const Score = 100
+// const scoreValue =100.3
+
+// const isLoggedIn = fasle
+// const outsideTemp = null 
+ 
+// let userEmail; undefined
+//  const id = Symbol('122')
+
+//  const anotherId= Symbol('123')
+//  const BigNumber = 4565454
+ 
+//  console.log(id === anotherId);
+
+// Reference data 3 types 
+// Array, Object, Function
+
+// const heros = ["imran" ,"khan""ali"]
+// this is Array
+
+// let MyObj = {
+//     id:123, 
+//   name:"imran",
+//   age:43,
+// }
+// this is Object
+
+// const myFunction = function(){
+//      console.log("hello world"); 
+// }
+// *************************************************
+
+const name = "imran ali"
+const age = 32
+console.log(`hello my name is ${name} and my age is${age}`)
+
+
